a1648f40f7e2573dafab4a58596271e5fb965bff
dennys3mf <dennys.marquez@tecsup.edu.pe>
2024-03-05 13:19:24 -0500

    Avance 01 del Encriptador: Diseño

---
+++ src/app.js
@@ -0,0 +1,4 @@
+const btnencriptar = document.getElementById('#boton-1');
+const btnDesencriptar = document.querySelector('#boton-2');
+const textArea = document.getElementsByClassName('AreaDeTexto');
+
