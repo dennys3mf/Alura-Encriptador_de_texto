7578a60c4b3567e1d9cda30835d19f7d147c0a96
dennys3mf <dennys.marquez@tecsup.edu.pe>
2024-03-10 16:14:06 -0500

    css: Responsive Desing

--- src/app.js
+++ src/app.js
@@ -2,3 +2,6 @@ const btnencriptar = document.getElementById('#boton-1');
 const btnDesencriptar = document.querySelector('#boton-2');
 const textArea = document.getElementsByClassName('AreaDeTexto');
 
+function encriptar() {
+  alert('Estas encriptando');
+}
